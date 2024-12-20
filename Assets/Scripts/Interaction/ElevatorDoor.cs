b42c2ecef96b8c663c161c38178cd3373b714997
BbaGoun <sscaios1@gmail.com>
2024-12-20 18:32:52 +0900

    엘레베이터 맵 선택창 나오는거 까진 겉모습으로 완성 시팔

--- Assets/Scripts/Interaction/ElevatorDoor.cs
+++ Assets/Scripts/Interaction/ElevatorDoor.cs
@@ -6,9 +6,44 @@ namespace ActionPart
 {
     public class ElevatorDoor : MonoBehaviour
     {
-        public void CloseDoor()
+        [SerializeField]
+        GameObject background;
+        [SerializeField]
+        GameObject map;
+        Animator animator;
+
+
+        private void Awake()
+        {
+            animator = GetComponent<Animator>();    
+        }
+
+        public void MapOn_CloseDoor()
+        {
+            background.SetActive(true);
+            map.SetActive(true);
+        }
+
+        public void MapOn_OpenDoor()
+        {
+
+        }
+
+        public void MapOff_Button()
+        {
+            animator.SetTrigger("isClosed");
+        }
+
+        public void MapOff_CloseDoor()
+        {
+            background.SetActive(false);
+            map.SetActive(false);
+        }
+
+        public void MapOff_OpenDoor()
         {
             this.gameObject.SetActive(false);
         }
     }
+
 }
