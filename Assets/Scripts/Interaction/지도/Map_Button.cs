9299260660facfccc3481c6b2a844a9c6316db09
BbaGoun <sscaios1@gmail.com>
2025-02-06 00:38:53 +0900

    지도 버튼 수정함
    선택된 지역의 지도 수정 필요, 설명 폰트 수정 필요

---
+++ Assets/Scripts/Interaction/지도/Map_Button.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ActionPart
+{
+    public class Map_Button : MonoBehaviour
+    {
+        private RectTransform rectTransform;
+        private Image image;
+        public Sprite mouseEnterSprite;
+        public Sprite mouseExitSprite;
+
+        public void Awake()
+        {
+            rectTransform = GetComponent<RectTransform>();
+            image = GetComponent<Image>();
+            image.alphaHitTestMinimumThreshold = 0.01f;
+            Color tempColor = Color.white;
+            tempColor.a = 0f;
+            image.color = tempColor;
+        }
+
+        public void OnMouseEnter()
+        {
+            image.sprite = mouseEnterSprite;
+            Color tempColor = Color.white;
+            tempColor.a = 1f;
+            image.color = tempColor;
+            rectTransform.localScale = Vector3.one;
+
+        }
+
+        public void OnMouseExit() 
+        {
+            image.sprite = mouseExitSprite;
+            Color tempColor = Color.white;
+            tempColor.a = 0f;
+            image.color = tempColor;
+            rectTransform.localScale = new Vector3(0.825f, 0.825f, 1f);
+        }
+    }
+}
